6d1af3a8f30cde9b37e280361b00941706e82a6d
PepeTonin <phavilat@hotmail.com>
2023-07-28 14:16:38 -0300

    fixed the cart item amount logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useState } from 'react'; 
+import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState();
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-  function showCartHandler() {
+  const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
-  function hideCartHandler(){
+  const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
 
   return (
     <CartProvider>
